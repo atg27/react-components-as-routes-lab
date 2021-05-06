d6f6394f1fd9d2bad8bf272e3e45935e0726ab32
Andrew <atg27@uw.edu>
2021-05-06 11:27:20 -0700

    commit tree 23a635cac8acfad0bb5bae39ab3757a4a8a2fac6

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { directors } from '../data';
+import { directors, movies } from '../data';
 
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors</h1>
+      {directors.map((director, index) => (
+        <div key={index}>
+          <h3>Name: {director.name}</h3>
+          <ul>Movies:
+            {director.movies.map((movie, index)=>(
+              <li key={index}>
+                  {movie}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,11 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div>
-      {/*{code here}*/}
-    </div>
+        <NavLink  to="/"> Home </NavLink>
+        <NavLink  to="/actors"> Actors </NavLink>
+        <NavLink  to="/directors"> Directors </NavLink>
+        <NavLink  to="/movies"> Movies </NavLink>
+            </div>
   );
 };
 
